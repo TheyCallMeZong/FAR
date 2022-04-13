e544c148503bf690b2ad0b3d23e4f58d7f647a2c
Semyon <savhenko10@mail.ru>
2022-04-13 21:33:44 +0700

    удаление файлов и директорий

---
+++ Far/Command/Tree.cs
@@ -0,0 +1,18 @@
+﻿namespace Far.Command
+{
+    /// <summary>
+    /// Дерево файлов и директорий
+    /// </summary>
+    public class Tree : ICommand<ConsoleKeyInfo>
+    {
+        public bool CanExecute(ConsoleKeyInfo item)
+        {
+            return item.Key == ConsoleKey.F9;
+        }
+
+        public bool Execute()
+        {
+            return false;
+        }
+    }
+}
